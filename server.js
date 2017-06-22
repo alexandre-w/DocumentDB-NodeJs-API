57ae0b035dbe50ac2a542354200d30a86eba81e8
Alexandre-w <alexandre@MacBook-Pro-de-Alexandre.local>
2017-06-22 23:21:18 +0200

    Add functions to create a new database and a collection

--- server.js
+++ server.js
@@ -5,10 +5,51 @@ console.log(config.primaryKey);
 var client =  new documentDbClient(config.uri, { "masterKey" : config.primaryKey});
 
 var databaseUrl = `dbs/${config.database.id}`;
-
-
 var collectionUrl = `${databaseUrl}/colls/${config.collection.id}`;
+var HttpStatusCodes = { NOTFOUND: 404};
 
 console.log("databaseUrl " + databaseUrl);
 console.log("collectionUrl " + collectionUrl);
 
+createCollectionIfnotExists();
+
+function createDatabaseIfNotExists(){
+  console.log("databaseUrl" + databaseUrl);
+
+  client.readDatabase(databaseUrl, (err, result) => {
+    if(err){
+      if (err.code == HttpStatusCodes.NOTFOUND) {
+        client.createDatabase(config.database, (err, created ) => {
+          if (err) {
+            console.log(JSON.stringify(err));
+          }else {
+            console.log("database created", JSON.stringify(created));
+          }
+        });
+      }else {
+        console.log("database created" , JSON.stringify(result));
+      }
+    }
+  });
+}
+
+function createCollectionIfnotExists(){
+  console.log("Getting collection: " + config.collection.id);
+
+  client.readCollection(collectionUrl, (err, result) => {
+    if (err) {
+      if (err.code == HttpStatusCodes.NOTFOUND) {
+        client.createCollection(databaseUrl, config.collection, {offerThroughput: 1000}, (e, c) => {
+          if (err) {
+            console.log("Error :" + JSON.stringify(e) );
+          }else {
+            console.log("Created : " + JSON.stringify(c) );
+          }
+        });
+      }
+    }else {
+      console.log("result collection:" + JSON.stringify(result) );
+    }
+  });
+
+}
